33b80e3da1cd2b0912fb6d224b8f1ce825bc8181
Polina <polly.poem@yandex.ru>
2024-11-30 00:32:04 +0300

    multible problems fixed

--- mpi_task.cpp
+++ mpi_task.cpp
@@ -1,6 +1,12 @@
+
+
+
 #define _USE_MATH_DEFINES
-#define TAU 0.001
-#define PAR
+#define T 0.0001
+#define L 1.0
+#define TAU T/20
+#define ITER 20.0
+
 #include <iostream>
 #include <string>
 #include <math.h>
@@ -12,15 +18,13 @@ using namespace std;
 
 struct Custom_vector
 {
-    vector<vector<double> > base;
-    int Nt;
+    vector<vector<vector<vector<double>>>> base;
     int N[3];
     vector<vector<double> > to_send;
     vector<vector<double> > to_recv;
-    Custom_vector(int nt, int* n) {
-        Nt = nt;
+    Custom_vector(int* n) {
         copy(n, n+3, N);
-        base = vector<vector<double> >(nt, vector<double>(n[0]*n[1]*n[2]));
+        base = vector<vector<vector<vector<double>>> >(ITER, vector<vector<vector<double>>>(n[0], vector<vector<double>>(n[1], vector<double>(n[2]))));
         for (int d = 0; d < 3; d++)
             for (int j = 0; j < 2; j++) {
                 to_send.push_back(vector<double>(n[(d+1)%3] * n[(d+2)%3]));
@@ -35,10 +39,7 @@ struct Custom_vector
         if (y == N[1]) return to_recv[3][z+x*N[0]];
         if (z == -1) return to_recv[4][y+x*N[0]];
         if (z == N[2]) return to_recv[5][y+x*N[0]];
-        return base[t][z+y*N[1]+x*N[0]*N[0]];
-    }
-    double get_normal(int t, int x, int y, int z) {
-        return base[t][z+y*N[1]+x*N[0]*N[0]];
+        return base[t][x][y][z];
     }
     void set(int t, int x, int y, int z, double value) {
         if (x == 0) to_send[0][z+y*N[1]] = value;
@@ -47,46 +48,47 @@ struct Custom_vector
         if (y == N[1]-1) to_send[3][z+x*N[0]] = value;
         if (z == 0) to_send[4][y+x*N[0]] = value;
         if (z == N[2]-1) to_send[5][y+x*N[0]] = value;
-        base[t][z+y*N[1]+x*N[0]*N[0]] = value;
+        base[t][x][y][z] = value;
     }
 
 };
 
 struct Counter {
     int N[3];
-    double L[3], h[3], base_vals[3], a_t;
+    double h[3], base_vals[3], a_t;
     Custom_vector grid;
     vector<double> shared_x;
-    Counter(int nt, int* n, double* l, int* bv) {
+    Counter(int* n, double* l, int* bv) {
         for (int i = 0; i<3; i++) {
             N[i] = n[i];
-            L[i] = l[i];
-            h[i] = l[i]/n[i];
-            bv[i] = bv[i]*h[i];
+            h[i] = l[i]/(i == 0? n[i]-1 : n[i]);
+            base_vals[i] = bv[i]*h[i];
         }
-        a_t = M_PI*sqrt(9/(L[0]*L[0]) + 4/(L[1]*L[1]) + 4/(L[2]*L[2]));
-        grid = Custom_vector(nt, N);
+        a_t = M_PI*sqrt(9/(L*L) + 4/(L*L) + 4/(L*L));
+        grid = Custom_vector(N);
     }
 
-    double Delta(int i, int j, int k, int n_t) {
+    double Delta(int n_t, int i, int j, int k) {
+
         return  (grid.get(n_t, i-1, j, k) - 2*grid.get(n_t, i, j, k)+ grid.get(n_t, i+1, j, k))/(h[0]*h[0]) +
                 (grid.get(n_t, i, j-1, k) - 2*grid.get(n_t, i, j, k)+ grid.get(n_t, i, j+1, k))/(h[1]*h[1]) +
                 (grid.get(n_t, i, j, k-1) - 2*grid.get(n_t, i, j, k)+ grid.get(n_t, i, j, k+1))/(h[2]*h[2]);
     }
 
-    double Count_u_from_grid (int i, int j, int k, int n_t) {
-        double delta = Delta(i, j, k, n_t-1);
-        return delta*TAU*TAU + 2*grid.get(n_t-1, i, j, k) - grid.get(n_t-1, i, j, k); 
+    double Count_u_from_grid(int n_t, int i, int j, int k) {
+        double delta = Delta(n_t-1, i, j, k);
+        return delta*TAU*TAU + 2*grid.get(n_t-1, i, j, k) - grid.get(n_t-2, i, j, k); 
     }
 
-    double Count_u_analitic (double x, double y, double z, int n_t) {
-        double t = n_t*TAU;
-        return sin(3*(M_PI/L[0])*(x+base_vals[0])) * sin(2*(M_PI/L[1])*(x+base_vals[0])) * sin(2*(M_PI/L[2])*(x+base_vals[0])) * cos(a_t*t + 4*M_PI);
+    double Count_u_analitic(int n_t, int i, int j, int k) {
+        double real_t = (n_t/ITER)*T;
+        return sin(3*(M_PI/L)*(i*h[0]+base_vals[0])) * 
+               sin(2*(M_PI/L)*(j*h[1]+base_vals[1])) * 
+               sin(2*(M_PI/L)*(k*h[2]+base_vals[2])) * cos(a_t*real_t + 4*M_PI);
     }
 };
 
 int main(int argc, char *argv[]) {
-#pragma region preparations
     int old_N;
     if (argc < 2)
         old_N  = 50;
@@ -99,21 +101,19 @@ int main(int argc, char *argv[]) {
             return 1;
         }
 
-    double L[] = {1, 1, 1};
-    double T=1, max;    
-    int err, numprocs, myid, Nt = 20;
+    int err, numprocs, myid;
     int dims[] = {0, 0, 0};
     int periods[3], neighbours_m[3], neighbours_p[3], coords[3];
     MPI_Comm COMM_CART;
-    if (err = MPI_Init(&argc, &argv)) { 
+    if (MPI_Init(&argc, &argv)) { 
         cout << "INIT ERROR!" << endl;
-        MPI_Abort(MPI_COMM_WORLD, err);
+        exit(1);
     } 
     MPI_Comm_size(MPI_COMM_WORLD,&numprocs); 
     MPI_Dims_create(numprocs, 3, dims);
-    if (err = MPI_Cart_create(MPI_COMM_WORLD, 3, dims, periods, 1, &COMM_CART)) {
+    if (MPI_Cart_create(MPI_COMM_WORLD, 3, dims, periods, 1, &COMM_CART)) {
         cout << "CART ERROR!" << endl;
-        MPI_Abort(MPI_COMM_WORLD, err);
+        exit(1);
     }
     MPI_Comm_rank(COMM_CART, &myid);
     if (old_N % dims[0] != 0 || old_N % dims[1] != 0 || old_N % dims[2] != 0) {
@@ -124,63 +124,89 @@ int main(int argc, char *argv[]) {
         MPI_Cart_shift(COMM_CART, dim, 1, &neighbours_m[dim], &neighbours_p[dim]);
     MPI_Cart_coords (COMM_CART, myid, 3, coords);
     int N[] = {old_N/dims[0], old_N/dims[1], old_N/dims[2]};
-    int base_vals[] = {N[0]*coords[0], N[1]*coords[1], N[1]*coords[1]};
+    double l[] = {L/dims[0], L/dims[1], L/dims[2]};
+    int base_vals[] = {N[0]*coords[0], N[1]*coords[1], N[2]*coords[2]};
     
     double start; 
-
     if (myid == 0)
         start  = MPI_Wtime();
-#pragma endregion
 
-    Counter counter(Nt, N, L, base_vals);
+    Counter counter(N, l, base_vals);
     for (int t = 0; t < 2; t++)
         for (int i = 0; i < N[0]; i++)
             for (int j = 0; j < N[1]; j++)
-                for (int k = 0; k < N[2]; k++)
-                    if (i == 0 && coords[0] == 0 || i == N[0] - 1 && coords[0] == dims[0]-1) 
+                for (int k = 0; k < N[2]; k++) {
+                    if (i == 0 && coords[0] == 0 || i == N[0]-1 && coords[0] == dims[0]-1)  {
                         counter.grid.set(t, i, j, k, 0);
-                    else 
-                        counter.grid.set(t, i, j, k, counter.Count_u_analitic(i, j, k, t));
-    
+                    }
+                    else {
+                        counter.grid.set(t, i, j, k, counter.Count_u_analitic(t, i, j, k));
+                    }
+                }
     MPI_Status status;
     
-    for (int t = 2; t < Nt; t++) {
-        for (int dim = 0; dim < 3; dim++){
+    for (int t = 2; t < ITER; t++) {
+        for (int dim = 0; dim < 3; dim++) {
+            MPI_Request requests[4];
+            MPI_Status status[4]; 
+
             if (neighbours_m[dim] != MPI_PROC_NULL && neighbours_p[dim] != MPI_PROC_NULL) {
-                MPI_Sendrecv(&(counter.grid.to_send[2*dim][0]), counter.grid.to_send[2*dim].size(), MPI_DOUBLE, neighbours_m[dim], 10,  
-                             &(counter.grid.to_recv[2*dim+1][0]), counter.grid.to_recv[2*dim+1].size(), MPI_DOUBLE, neighbours_p[dim], 10, COMM_CART, &status);
-                MPI_Sendrecv(&(counter.grid.to_send[2*dim+1][0]), counter.grid.to_send[2*dim+1].size(), MPI_DOUBLE, neighbours_p[dim], 11,  
-                             &(counter.grid.to_recv[2*dim][0]), counter.grid.to_recv[2*dim].size(), MPI_DOUBLE, neighbours_m[dim], 11, COMM_CART, &status);
-            }
+                MPI_Isend(&(counter.grid.to_send[2 * dim][0]), counter.grid.to_send[2 * dim].size(), MPI_DOUBLE, 
+                        neighbours_m[dim], 10, COMM_CART, &requests[0]);
+                MPI_Irecv(&(counter.grid.to_recv[2 * dim + 1][0]), counter.grid.to_recv[2 * dim + 1].size(), MPI_DOUBLE, 
+                        neighbours_p[dim], 10, COMM_CART, &requests[1]);
+
+                MPI_Waitall(2, requests, status);
+
+                MPI_Isend(&(counter.grid.to_send[2 * dim + 1][0]), counter.grid.to_send[2 * dim + 1].size(), MPI_DOUBLE, 
+                        neighbours_p[dim], 11, COMM_CART, &requests[2]);
+                MPI_Irecv(&(counter.grid.to_recv[2 * dim][0]), counter.grid.to_recv[2 * dim].size(), MPI_DOUBLE, 
+                        neighbours_m[dim], 11, COMM_CART, &requests[3]);
+
+                MPI_Waitall(2, &requests[2], &status[2]);
+            } 
             else if (neighbours_m[dim] != MPI_PROC_NULL) {
-                MPI_Send(&(counter.grid.to_send[2*dim][0]), counter.grid.to_send[2*dim].size(), MPI_DOUBLE, neighbours_m[dim], 10, COMM_CART); 
-                MPI_Recv(&(counter.grid.to_recv[2*dim][0]), counter.grid.to_recv[2*dim].size(), MPI_DOUBLE, neighbours_m[dim], 11, COMM_CART, &status);         
-            }
+                MPI_Isend(&(counter.grid.to_send[2 * dim][0]), counter.grid.to_send[2 * dim].size(), MPI_DOUBLE, 
+                        neighbours_m[dim], 10, COMM_CART, &requests[0]);
+                MPI_Irecv(&(counter.grid.to_recv[2 * dim][0]), counter.grid.to_recv[2 * dim].size(), MPI_DOUBLE, 
+                        neighbours_m[dim], 11, COMM_CART, &requests[1]);
+
+                MPI_Waitall(2, requests, status);
+            } 
             else if (neighbours_p[dim] != MPI_PROC_NULL) {
-                MPI_Recv(&(counter.grid.to_recv[2*dim+1][0]), counter.grid.to_recv[2*dim+1].size(), MPI_DOUBLE, neighbours_p[dim], 10, COMM_CART, &status);    
-                MPI_Send(&(counter.grid.to_send[2*dim+1][0]), counter.grid.to_send[2*dim+1].size(), MPI_DOUBLE, neighbours_p[dim], 11, COMM_CART);  
+                MPI_Irecv(&(counter.grid.to_recv[2 * dim + 1][0]), counter.grid.to_recv[2 * dim + 1].size(), MPI_DOUBLE, 
+                        neighbours_p[dim], 10, COMM_CART, &requests[0]);
+                
+                MPI_Wait(&requests[0], &status[0]);
+                MPI_Isend(&(counter.grid.to_send[2 * dim + 1][0]), counter.grid.to_send[2 * dim + 1].size(), MPI_DOUBLE, 
+                        neighbours_p[dim], 11, COMM_CART, &requests[1]);
+                
+                MPI_Wait(&requests[1], &status[1]);
             }
         }
+
         for (int i = 0; i < N[0]; i++)
             for (int j = 0; j < N[1]; j++)
                 for (int k = 0; k < N[2]; k++)
-                    if (i == 0 || i == N[0] - 1)
+                    if (i == 0 && coords[0] == 0 || i == N[0]-1 && coords[0] == dims[0]-1) 
                         counter.grid.set(t, i, j, k, 0);
                     else {
-                        counter.grid.set(t, i, j, k, counter.Count_u_from_grid(i, j, k, t));
+                        counter.grid.set(t, i, j, k, counter.Count_u_from_grid(t, i, j, k));
                     }
     }
 
     double my_max = 0, overall_max = 0;
     {
-        for (int t = 0; t < Nt; t++)
-        for (int i = 0; i < N[0]; i++)
-            for (int j = 0; j < N[1]; j++)
-                for (int k = 0; k < N[2]; k++) {
-                        double my = counter.grid.get(t, i, j, k), ideal = counter.Count_u_analitic(i, j, k, t);
-                        double diff = my > ideal ? my-ideal : ideal-my;
-                        if (diff > my_max)
-                            my_max = diff; 
+        for (int t = 0; t < ITER; t++)
+            for (int i = 1; i < N[0]-1; i++)
+                for (int j = 0; j < N[1]; j++)
+                    for (int k = 0; k < N[2]; k++) {
+                            double my = counter.grid.get(t, i, j, k);
+                            double ideal = counter.Count_u_analitic(t, i, j, k);
+                            double diff = my > ideal ? my-ideal : ideal-my;
+                            if (diff > my_max){
+                                my_max = diff; 
+                            }
                     }
         MPI_Reduce(&my_max, &overall_max, 1, MPI_DOUBLE, MPI_MAX, 0, COMM_CART);
 
--- posl_task.cpp
+++ posl_task.cpp
@@ -1,6 +1,7 @@
 #define _USE_MATH_DEFINES
-#define TAU 0.0001
-#define PAR
+#define T 0.0001
+#define TAU T/20
+#define ITER 20.0
 
 #include <iostream>
 #include <string>
@@ -15,11 +16,10 @@ using namespace std;
 struct Custom_vector
 {
     vector<vector<double> > base;
-    int Nt, N;
-    Custom_vector(int nt, int n) {
-        Nt = nt;
+    int N;
+    Custom_vector(int n) {
         N = n;
-        base = vector<vector<double> >(nt, vector<double>(n * n * n));
+        base = vector<vector<double> >(ITER, vector<double>(n * n * n));
     }
     Custom_vector(){}
     double get(int t, int x, int y, int z) {
@@ -35,7 +35,7 @@ struct Counter {
     double Lx, Ly, Lz, hx, hy, hz, a_t;
     Custom_vector grid;
 
-    Counter(int nt, int n, double lx, double ly, double lz) {
+    Counter(int n, double lx, double ly, double lz) {
         N = n;
         Lx = lx;
         Ly = ly;
@@ -43,34 +43,34 @@ struct Counter {
         hx = Lx/N;
         hy = Ly/N;
         hz = Lz/N;
-        a_t = M_PI*sqrt(9/(Lx*Lx) + 4/(Ly*Ly) + 4/(Lz*Lz));
-        grid = Custom_vector(nt, n);
+        a_t = M_PI*sqrt(9.0/(Lx*Lx) + 4.0/(Ly*Ly) + 4.0/(Lz*Lz));
+        grid = Custom_vector(n+1);
     }
 
-    double Delta(int i, int j, int k, int n_t) {
-        int jm1 = j == 0 ? N-1 : j-1;
-        int jp1 = j == N-1 ? 0 : j+1;
-        int km1 = k == 0 ? N-1 : k-1;
-        int kp1 = k == N-1 ? 0 : k+1;
-        return  (grid.get(n_t, i-1, j, k) - 2*grid.get(n_t, i, j, k)+ grid.get(n_t, i+1, j, k))/(hx*hx) +
-                (grid.get(n_t, i, jm1, k) - 2*grid.get(n_t, i, j, k)+ grid.get(n_t, i, jp1, k))/(hy*hy) +
-                (grid.get(n_t, i, j, km1) - 2*grid.get(n_t, i, j, k)+ grid.get(n_t, i, j, kp1))/(hz*hz);
+    double Delta(int n_t, int i, int j, int k) {
+        int jm1 = j == 0 ? N : j-1;
+        int jp1 = j == N ? 0 : j+1;
+        int km1 = k == 0 ? N : k-1;
+        int kp1 = k == N ? 0 : k+1;
+        return  (double)(grid.get(n_t, i-1, j, k) - 2*grid.get(n_t, i, j, k)+ grid.get(n_t, i+1, j, k))/(hx*hx) +
+                (double)(grid.get(n_t, i, jm1, k) - 2*grid.get(n_t, i, j, k)+ grid.get(n_t, i, jp1, k))/(hy*hy) +
+                (double)(grid.get(n_t, i, j, km1) - 2*grid.get(n_t, i, j, k)+ grid.get(n_t, i, j, kp1))/(hz*hz);
     }
 
-    double Count_u_from_grid (int i, int j, int k, int n_t) {
-        double delta = Delta(i, j, k, n_t-1);
-        return delta*TAU*TAU + 2*grid.get(n_t-1, i, j, k) - grid.get(n_t-1, i, j, k); 
+    double Count_u_from_grid (int n_t, int i, int j, int k) {
+        double delta = Delta(n_t-1, i, j, k);
+        return delta*TAU*TAU + 2*grid.get(n_t-1, i, j, k) - grid.get(n_t-2, i, j, k); 
     }
 
-    double Count_u_analitic (double x, double y, double z, int n_t) {
-        double t = n_t*TAU;
-        return sin(3*(M_PI/Lx)*x) * sin(2*(M_PI/Ly)*y) * sin(2*(M_PI/Lz)*z) * cos(a_t*t + 4*M_PI);
+    double Count_u_analitic (int n_t, int i, int j, int k) {
+        double real_t = (n_t/ITER)*T;
+        return sin(3*(M_PI/Lx)*i*hx) * sin(2*(M_PI/Ly)*j*hy) * sin(2*(M_PI/Lz)*k*hz) * cos(a_t*real_t + 4*M_PI);
     }
 };
 
 int main(int argc, char const *argv[]) {
     int N;
-    double Lx=1, Ly=1, Lz=1, T=1, max;    
+    double Lx=1, Ly=1, Lz=1;    
     if (argc < 2)
         N  = 50;
     else {
@@ -82,44 +82,43 @@ int main(int argc, char const *argv[]) {
             return 1;
         }
     }
-    int Nt = 20;
     auto start = chrono::system_clock::now();
-    Counter counter(Nt, N, Lx, Ly, Lz);
+    Counter counter(N, Lx, Ly, Lz);
 
     for (int t = 0; t < 2; t++)
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < N; j++)
-                for (int k = 0; k < N; k++)
-                    if (i == 0 || i == N - 1) 
+        for (int i = 0; i <= N; i++)
+            for (int j = 0; j <= N; j++)
+                for (int k = 0; k <= N; k++) 
+                    if (i == 0 || i == N) 
                         counter.grid.set(t, i, j, k, 0);
                     else 
-                        counter.grid.set(t, i, j, k, counter.Count_u_analitic(i, j, k, t));
+                        counter.grid.set(t, i, j, k, counter.Count_u_analitic(t, i, j, k));
     
-    for (int t = 2; t < Nt; t++) {
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < N; j++)
-                for (int k = 0; k < N; k++)
-                    if (i == 0 || i == N - 1)
+    for (int t = 2; t < ITER; t++) {
+        for (int i = 0; i <= N; i++)
+            for (int j = 0; j <= N; j++)
+                for (int k = 0; k <= N; k++)
+                    if (i == 0 || i == N)
                         counter.grid.set(t, i, j, k, 0);
-                    else {
-                        counter.grid.set(t, i, j, k, counter.Count_u_from_grid(i, j, k, t));
-                    }
+                    else
+                        counter.grid.set(t, i, j, k, counter.Count_u_from_grid(t, i, j, k));
     }
 
     double overall_max = 0;
-    for (int t = 0; t < Nt; t++)
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < N; j++)
-                for (int k = 0; k < N; k++) {
-                    double my = counter.grid.get(t, i, j, k), ideal = counter.Count_u_analitic(i, j, k, t);
+    for (int t = 0; t < ITER; t++){
+        for (int i = 0; i <= N; i++)
+            for (int j = 0; j <= N; j++)
+                for (int k = 0; k <= N; k++) {
+                    double my = counter.grid.get(t, i, j, k);
+                    double ideal = counter.Count_u_analitic(t, i, j, k);
                     double diff = my > ideal ? my-ideal : ideal-my;
                     if (diff > overall_max)
                         overall_max = diff; 
                 }
+    }
 
     auto end = chrono::system_clock::now();
     std::chrono::duration<double> elapsed_seconds = end-start;
     cout << "Work took " << elapsed_seconds.count() << " seconds, max error - " << scientific << overall_max << endl;
-    system("pause");
     return 0;
 }
